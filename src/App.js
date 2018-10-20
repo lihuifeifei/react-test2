52d60a546c064f510e497f4d0540a0d04fbcb7cf
wangle <wangle@nibirutech.com>
2018-10-20 20:57:15 +0800

    lihui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+class Button extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        var themeColor=this.props.themeColor;
+        return(
+            <div >
+                <button style={{color:themeColor}} onClick={()=>this.props.handleClick('red')}>红色</button>
+                <button style={{color:themeColor}} onClick={()=>this.props.handleClick('green')}>绿色</button>
+            </div>
+        )
+    }
+}
+
+class Title extends Component {
+    constructor (){
+        super()
+    }
+    render () {
+        const themeColor=this.props.themeColor;
+        return (
+            <h2 style={{color:themeColor}}>标题</h2>
+        )
+    }
+}
 class App extends Component {
+    constructor (props){
+        super(props)
+        this.state={
+            themeColor:''
+        }
+    }
+    handleClick(color){
+        this.setState(
+            {themeColor:color}
+        )
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <Title themeColor={this.state.themeColor}></Title>
+          <Button themeColor={this.state.themeColor} handleClick={
+              (color)=>this.handleClick(color)}></Button>
       </div>
     );
   }
